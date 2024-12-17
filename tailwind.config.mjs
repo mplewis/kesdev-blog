6a0837e22c334356af239efdf9822e011c16e168
Matt Lewis <matt@mplewis.com>
2024-12-17 00:13:43 -0700

    feat: working blog

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      serif: ["Taviraj", "serif"],
+      handwritten: ["Sriracha", "sans-serif"],
+    },
+    fontWeight: {
+      normal: 300,
+      bold: 500,
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
