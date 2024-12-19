452fb32ff63589425d7cb2ad260081a0d66b6def
Matt Lewis <matt@mplewis.com>
2024-12-19 01:02:24 -0700

    tidy: unhack types in rss.xml

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,7 @@ import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
 import { excerpt } from "../logic/excerpt";
+import type { Post } from "../content.config";
 
 export async function GET(context: APIContext) {
   if (!context.site) throw new Error("site is undefined");
@@ -13,13 +14,17 @@ export async function GET(context: APIContext) {
     description: "Make. Break. Repeat.",
     customData: `<language>en-us</language>`,
     items: posts
-      .sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
-      .map((post) => ({
-        title: post.data.title,
-        link: `${context.site}${post.data.slug}`,
-        pubDate: post.data.date,
-        description: excerpt(post.rendered!.html), // HACK
-        content: post.rendered!.html,
-      })),
+      .sort((a: Post, b: Post) => b.data.date.getTime() - a.data.date.getTime())
+      .map((post: Post) => {
+        if (!post.rendered)
+          throw new Error(`Post ${post.data.slug} has no rendered content`);
+        return {
+          title: post.data.title,
+          link: `${context.site}${post.data.slug}`,
+          pubDate: post.data.date,
+          description: excerpt(post.rendered.html),
+          content: post.rendered.html,
+        };
+      }),
   });
 }
